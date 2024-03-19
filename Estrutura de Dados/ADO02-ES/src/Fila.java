d8b1438571e6fa1d76d35919878aef844a1e7417
Pelifefe <felippeapedrosa2004@gmail.com>
2024-03-19 13:34:36 -0300

    Atividade de ADO02 para terminar

---
+++ Estrutura de Dados/ADO02-ES/src/Fila.java
@@ -0,0 +1,94 @@
+public class Fila {
+    private Pessoa [] fila;
+    private int qtsPessoas;
+    private int qtsPreferenciais;
+    public Fila(int tamanhoFila) {
+        this.fila = new Pessoa[tamanhoFila];
+        this.qtsPessoas = 0;
+        this.qtsPreferenciais = 0;
+    }
+    public Fila(){
+        this(10);
+    }
+    public void enfileirar(Pessoa pessoa){
+        aumentaCapacidade();
+        if(qtsPessoas == 0){
+            fila[0]= pessoa;
+            qtsPessoas++;
+        }else{
+            if(!pessoa.isPreferencial()){
+                fila[qtsPessoas] = pessoa;
+                qtsPessoas++;
+            }else{
+                int i;
+                for(i = qtsPessoas - 1;i >= 0; i--){
+                    if(!fila[i].isPreferencial()){
+                        fila[i + 1] = fila[i];
+                    }else{
+                        break;
+                    }
+                }
+                fila[i + 1] = pessoa;
+                qtsPessoas++;
+            }
+        }
+    }
+    private void aumentaCapacidade(){
+        if(this.qtsPessoas == this.fila.length){
+            Pessoa[] filaNova = new Pessoa[this.fila.length + 1];
+            for (int i=0; i<this.fila.length; i++){
+                filaNova[i] = this.fila[i];
+            }
+            this.fila = filaNova;
+        }
+    }
+    public int qtsPessoas(){
+        return this.qtsPessoas();
+    }
+
+    public void desenfileirar() {
+        if(fila[0].isPreferencial()){
+            if(!(qtsPreferenciais == 3)){
+                qtsPreferenciais++;
+                moverFila(0);
+                qtsPessoas--;
+            }else{
+                int i;
+                for(i = 0; i < qtsPessoas; i++){
+                    if(!fila[i].isPreferencial()){
+                        qtsPreferenciais = 0;
+                        moverFila(i);
+                        qtsPessoas--;
+                        break;
+                    }
+                }
+            }
+        }else{
+            qtsPreferenciais = 0;
+            moverFila(0);
+            qtsPessoas--;
+        }
+    }
+    private void moverFila(int posicao){
+        for (int i = posicao; i < qtsPessoas - 1; i++){
+            fila[i] = fila[i+1];
+        }
+    }
+    @Override
+    public String toString(){
+        StringBuilder s = new StringBuilder();
+        s.append("[");
+
+        for(int i =0; i<this.qtsPessoas-1; i++){
+            s.append(this.fila[i].getNome() + ", é preferencial " + this.fila[i].isPreferencial());
+            s.append("| ");
+        }
+        if(this.qtsPessoas> 0){
+            s.append(this.fila[qtsPessoas-1].getNome() + ", é preferencial " + this.fila[qtsPessoas-1].isPreferencial());
+
+        }
+        s.append("]");
+        return s.toString();
+    }
+}
+
---
+++ Estrutura de Dados/ADO02-ES/src/Main.java
@@ -0,0 +1,52 @@
+import java.security.PKCS12Attribute;
+
+public class Main {
+    public static void main(String[] args) {
+        Pessoa p1 = new Pessoa();
+        p1.setNome("Osvaldo");
+        p1.setPreferencial(true);
+        Pessoa p2 = new Pessoa();
+        p2.setNome("Maria");
+        p2.setPreferencial(true);
+        Pessoa p3 = new Pessoa();
+        p3.setNome("Paula");
+        p3.setPreferencial(false);
+        Pessoa p4 = new Pessoa();
+        p4.setNome("Diana");
+        p4.setPreferencial(false);
+        Pessoa p5 = new Pessoa();
+        p5.setNome("Vitor");
+        p5.setPreferencial(false);
+        Pessoa p6 = new Pessoa();
+        p6.setNome("Godofredo");
+        p6.setPreferencial(true);
+        Pessoa p7 = new Pessoa();
+        p7.setNome("Nádia");
+        p7.setPreferencial(true);
+
+        Fila fila = new Fila();
+        fila.enfileirar(p1);
+        System.out.println("Primeira pessoa adicionada na fila: ");
+        System.out.println(fila.toString());
+
+        fila.enfileirar(p3);
+        fila.enfileirar(p2);
+        fila.enfileirar(p5);
+        fila.enfileirar(p4);
+        System.out.println("4 pessoas adicionadas na fila: ");
+        System.out.println(fila.toString());
+        fila.enfileirar(p6);
+        fila.enfileirar(p7);
+        System.out.println("2 pessoas preferenciais adicionadas");
+        System.out.println(fila.toString());
+
+        fila.desenfileirar();
+        System.out.println("1 pessoas desifileirada: ");
+        System.out.println(fila.toString());
+        fila.desenfileirar();
+        fila.desenfileirar();
+        fila.desenfileirar();
+        System.out.println("Mais 3 pessoas desinfileiradas: ");
+        System.out.println(fila.toString());
+    }
+}
